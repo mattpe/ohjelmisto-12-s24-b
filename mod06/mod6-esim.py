799c3f20767304e907d709b2ad87dbfa05a4190b
mattpe <mattpe@metropolia.fi>
2024-09-03 15:26:31 +0300

    add more mod6 function examples

--- mod06/mod6-esim.py
+++ mod06/mod6-esim.py
@@ -70,3 +70,51 @@ while command != "lopeta":
     else:
         print("En ymmärrä komentoa. Yritä uudestaan, kiitti.")
 print("Ohjelma sammutettu.")
+
+
+# listat ja funktiot, funktio ottaa parametrina listan lukuja ja laskee
+# ja palauttaa niiden summan
+def calculate_sum(numbers):
+    #print(numbers)
+    total_sum = 0
+    # kaksi tapaa tehdä for-loop listan käsittelyyn
+    for i in range(len(numbers)):
+        total_sum = total_sum + numbers[i]
+        numbers[i] = 0 # nollataan listan käsiteltävä alkio ihan vaan huvikseen
+    #print(numbers)
+    #for num in numbers:
+    #    total_sum = total_sum + num
+    return total_sum
+
+nums = [3, 4, 5]
+print(nums)
+print(calculate_sum(nums))
+# funktio muokkasi samaa listaa, mihin pääohjelman nums muuttuja viittaa
+print(nums)
+#print(calculate_sum([3, 4, 5, 10]))
+
+
+# extra: vaihtuva määrä parametreja
+# * tekee kaikista syötetyistä parametreista (arvoista) listan
+# ja sijoittaa listan numbers-muuttujaan
+def calculate_sum2(*numbers):
+    total_sum = 0
+    for num in numbers:
+        total_sum = total_sum + num
+    return total_sum
+
+print(calculate_sum2(2, 3, 8, -10, 4.67))
+
+# extra: nimetyt parametrit ja oletusarvot
+# yksikertainen laskin, jolle voi antaa 2-3 parametria
+def calculate2(number1, number2, calc_type="sum"):
+    if calc_type == "sum":
+        return number1 + number2
+    elif calc_type == "division":
+        return number1 / number2
+    #return None # oletustoiminnallisuus
+
+# useita tapoja syötää parametrien arvot, joko järjestyksessä tai nimettynä
+print(calculate2(2.4, 3.5))
+print(calculate2(2.4, 3.5, "division"))
+print(calculate2(calc_type="division", number2=2.4, number1=3.5))
