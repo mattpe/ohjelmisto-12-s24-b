a7e0f010a511792fbcf61cb45df137bb99f85c4d
mattpe <mattpe@metropolia.fi>
2024-10-24 15:14:41 +0300

    add assosiation example

--- mod10/mod10-esim.py
+++ mod10/mod10-esim.py
@@ -1,10 +1,19 @@
+import random
+
 class Autotalli:
     def __init__(self):
         self.autot = []
     def auto_sisaan(self, auto):
+        # tarkistetaan, ettei auto ole jo tallissa
+        # parempi tapa olisi muuttaa lista joukoksi, jolloin duplikaatteja ei sallita
+        for a in self.autot:
+            if a == auto: # => True jos viittaavat samaan olioon
+                return
         self.autot.append(auto)
-    def auto_ulos(self):
-        return
+
+    def auto_ulos(self, auto):
+        self.autot.remove(auto)
+
     def tulosta_inventaario(self):
         print("Autot tallissa:")
         for auto in self.autot:
@@ -20,8 +29,10 @@ class Kuljettaja:
         print(f"Olen {self.nimi}, {self.ika}, ja ajan autoa {self.auto.rek_nro}:")
         self.auto.kiihdyta(40)
         print(self.auto.nopeus)
+        self.auto.kulje(1)
         self.auto.kiihdyta(140)
         print(self.auto.nopeus)
+        self.auto.kulje(0.1)
         self.auto.kiihdyta(-250)
         self.auto.tulosta_ominaisuudet()
 
@@ -29,12 +40,14 @@ class Auto:
     def __init__(self, rek_nro, huippunopeus):
         self.rek_nro = rek_nro
         self.nopeus = 0
+        self.matka = 0
         self.huippunopeus = huippunopeus
         self.kuljettaja = "Räikkönen"
+        print(f"Auto luotu {rek_nro}, huiput: {huippunopeus}.")
 
     def tulosta_ominaisuudet(self):
         print(f"{self.rek_nro}, huippunopeus: {self.huippunopeus}")
-        print(f"Tämänhetkinen nopeus: {self.nopeus}")
+        print(f"Nopeus: {self.nopeus}, matkamittari: {self.matka}")
 
     def kiihdyta(self, nopeuden_muutos):
         self.nopeus = self.nopeus + nopeuden_muutos
@@ -44,6 +57,10 @@ class Auto:
         # auto pysähtyy, jos nopeus alle 0
         if self.nopeus < 0:
             self.nopeus = 0
+
+    def kulje(self, aika):
+        # aika tunneissa
+        self.matka += aika * self.nopeus
 
 # luodaan oliot ja sijoitetaan viittauksen niihin muuttujiin
 a1 = Auto("ABC-123", 142)
@@ -59,6 +76,15 @@ talli = Autotalli()
 talli.auto_sisaan(a1)
 talli.tulosta_inventaario()
 talli.auto_sisaan(a2)
+#talli.auto_sisaan(a2)
+#talli.auto_ulos(a1)
+
+talli.tulosta_inventaario()
+
+# luodaan 10 erilaista auto-oliota autotalliin
+for i in range(10):
+    uusi_auto = Auto(f"ABC-{i+1}", random.randint(100, 200))
+    talli.auto_sisaan(uusi_auto)
 talli.tulosta_inventaario()
 
 #a2.kiihdyta(5)
