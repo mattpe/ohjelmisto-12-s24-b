7cba0089545ce60d856346712773fbc3c35a8072
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2024-11-21 12:36:26 +0200

    add js-mod03 assignment 5

---
+++ javascript/js-mod3/t5/5.js
@@ -0,0 +1,169 @@
+'use strict';
+const picArray = [
+  {
+    title: 'Title 1',
+    caption: 'Caption 1',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic1.jpg',
+      medium: 'thumbnails/pic1.jpg',
+    },
+  },
+  {
+    title: 'Title 2',
+    caption: 'Caption 2',
+    description:
+        'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    image: {
+      large: 'img/pic2.jpg',
+      medium: 'thumbnails/pic2.jpg',
+    },
+  },
+  {
+    title: 'Title 3',
+    caption: 'Caption 3',
+    description:
+        'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    image: {
+      large: 'img/pic3.jpg',
+      medium: 'thumbnails/pic3.jpg',
+    },
+  },
+  {
+    title: 'Title 4',
+    caption: 'Caption 4',
+    description:
+        'Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    image: {
+      large: 'img/pic4.jpg',
+      medium: 'thumbnails/pic4.jpg',
+    },
+  },
+  {
+    title: 'Title 5',
+    caption: 'Caption 5',
+    description:
+        'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. ',
+    image: {
+      large: 'img/pic5.jpg',
+      medium: 'thumbnails/pic5.jpg',
+    },
+  },
+  {
+    title: 'Title 6',
+    caption: 'Caption 6',
+    description:
+        'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh.',
+    image: {
+      large: 'img/pic6.jpg',
+      medium: 'thumbnails/pic6.jpg',
+    },
+  },
+  {
+    title: 'Title 7',
+    caption: 'Caption 7',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic7.jpg',
+      medium: 'thumbnails/pic7.jpg',
+    },
+  },
+  {
+    title: 'Title 8',
+    caption: 'Caption 8',
+    description:
+        'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic8.jpg',
+      medium: 'thumbnails/pic8.jpg',
+    },
+  },
+  {
+    title: 'Title 9',
+    caption: 'Caption 9',
+    description:
+        'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic9.jpg',
+      medium: 'thumbnails/pic9.jpg',
+    },
+  },
+];
+
+/*
+const main = document.querySelector('main');
+console.log(main);
+
+main.addEventListener('click', function(event) {
+  alert('Klikkasit mainia!!');
+})
+ */
+
+
+
+
+// const section = document.querySelector('#pictures');
+const section = document.getElementById('pictures');
+
+for (const pic of picArray) {
+  console.log(pic.image);
+
+  // create article
+  let a = document.createElement('article');
+  a.classList.add('card');
+  section.appendChild(a);
+
+      // create h2
+      let h = document.createElement('h2');
+      h.innerText = pic.title;
+      a.appendChild(h);
+
+      // create figure
+      let f = document.createElement('figure');
+      a.appendChild(f);
+
+          // create img
+          let i = document.createElement('img');
+          // Vaihtoehtoisia tapoja lisätä attribuutit
+          i.alt = pic.title;
+          i.src = pic.image.medium;
+          //i.setAttribute('src', pic.image.medium);
+          f.appendChild(i);
+
+          // create figcaption
+          let fc = document.createElement('figcaption');
+          fc.innerText = pic.caption;
+          f.appendChild(fc);
+
+      // create p
+      let p = document.createElement('p');
+      p.innerText = pic.description;
+      a.appendChild(p);
+
+      // loopissa voidaan lisätä jokaiselle erikseen
+      f.addEventListener('click', function(event) {
+      alert(`Klikkasit kuvaa, otsikolla ${pic.title}`);
+    });
+
+}
+
+// tai ihan vaan pääohjelmassa yksittäiselle elementille
+const footer = document.querySelector('footer');
+footer.addEventListener('mouseover', function(event) {
+  alert('Veit hiiren footerin päälle');
+});
+
+
+// add your code here
+/*
+<article className="card">
+  <h2>title_from_picArray</h2>
+  <figure>
+    <img src="medium_image_from_picArray" alt="title_from_picArray"/>
+    <figcaption>caption_from_picarray</figcaption>
+  </figure>
+  <p>description_from_picArray</p>
+</article>
+ */
