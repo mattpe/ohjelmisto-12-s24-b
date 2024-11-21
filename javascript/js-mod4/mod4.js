acb76ef4bb93e32bcc96f5249f673880fb8c1217
mattpe <mattpe@metropolia.fi>
2024-11-21 15:39:17 +0200

    add ajax-py esimerkki

--- javascript/js-mod4/mod4.js
+++ javascript/js-mod4/mod4.js
@@ -1,7 +1,8 @@
 'use strict';
 console.log('script start');
 
-
+// hakee pics.json-tiedoston samalta palvelmilta, millä tämä js-tiedosto on
+// käsittelee taulukkodatan ja tekee kuva-elementit DOMiin
 async function fetchImages() {
     const response = await fetch('pics.json');
     console.log('pic response', response);
@@ -19,16 +20,47 @@ async function fetchImages() {
 }
 fetchImages();
 
-// Chuck norris api esimerkki
+
+// Chuck norris api -esimerkki ja virheenkäsittely
 async function getAJoke() {
-    const response = await fetch('https://api.chucknorris.io/jokes/random');
-    const joke = await response.json();
-    console.log('vitsi: ', joke.value);
-    document.querySelector('#joke').textContent = joke.value;
+    const outputElem = document.querySelector('#joke');
+    try {
+        const response = await fetch('https://api.chucknorris.io/jokes/random');
+        console.log('joke response', response);
+        if (!response.ok) {
+            // status koodi jotain muuta kuin ok (ei 2xx)
+            throw new Error(response.status.toString());
+        }
+        const joke = await response.json();
+        console.log('vitsi: ', joke.value);
+        outputElem.textContent = joke.value;
+    } catch (error) {
+        console.error(error);
+        outputElem.textContent = "Vitsin haku epäonnistui.";
+    }
 }
+
 // haetaan vitsi vain käyttäjän pyynnöstä
 document.querySelector('#get-joke')
     .addEventListener('click', getAJoke);
+
+// Esimerkki oman flask-serverin käytöstä
+
+async function fetchAirport(icao) {
+    // TODO: lisää virheenkäsittely
+    const response = await fetch('http://localhost:5000/airport/' + icao);
+    const airport = await response.json();
+    //console.log('airport data', airport);
+    // TODO: add data to dom (näytä käyttäjälle)
+}
+// fetchAirport('efhk');
+const form = document.querySelector('#airport-form');
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    // haetaan viittaus input-elementtii ja sen arvo (käyttäjän syöte)
+    const userInput = form.querySelector('input').value;
+    fetchAirport(userInput);
+});
 
 
 document.querySelector('#joke').textContent = "odotetaan vieläkin";
